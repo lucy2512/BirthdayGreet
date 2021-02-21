fce78d2b1b95e0c9df76c810c1ca4d298f92602b
lucy2512 <mail4parthib@gmail.com>
2021-02-21 20:35:04 +0530

    complete till toast

--- app/src/main/java/com/parthib/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/parthib/birthdaygreet/MainActivity.kt
@@ -2,10 +2,20 @@ package com.parthib.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun birthdaygreet(view: View) {
+
+        Toast.makeText(this,"Great!",Toast.LENGTH_LONG).show()
     }
 }
