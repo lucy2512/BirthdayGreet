b5e09823db6d466fc027f7cd8e9760c7e5834fe4
lucy2512 <mail4parthib@gmail.com>
2021-02-22 22:13:34 +0530

    intent passing

--- app/src/main/java/com/parthib/birthdaygreet/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/parthib/birthdaygreet/BirthdayGreetingActivity.kt
@@ -2,14 +2,21 @@ package com.parthib.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class BirthdayGreetingActivity : AppCompatActivity() {
     companion object{
-        val NAME_EXTRA="name_extra"
+        const val NAME_EXTRA="name_extra"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
+
+        val intent=getIntent()
+        val name=intent.getStringExtra("name")
+        val name1 = findViewById<TextView>(R.id.textView)
+        name1.text="Happy Birthday"+ name
+
     }
-    val name=intent.getStringExtra(NAME_EXTRA)
 }
--- app/src/main/java/com/parthib/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/parthib/birthdaygreet/MainActivity.kt
@@ -13,17 +13,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
-    fun birthdaygreet(view: View) {
-        val editText = findViewById<EditText>(R.id.lucy)
-        val lucy = editText.text
-        Toast.makeText(this,"Name is $lucy",Toast.LENGTH_LONG).show()
+    fun CreateBirthdayCard(view: View) {
 
-        val intent= Intent(this,BirthdayGreetingActivity::class.java )
-        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA,lucy)
+        val editText = findViewById<EditText>(R.id.lucy)
+        val name = editText.text
+        Toast.makeText(this,"Name is $name",Toast.LENGTH_SHORT).show()
+        val intent=Intent(this,BirthdayGreetingActivity::class.java)
+        intent.putExtra("name",name)
         startActivity(intent)
+
+
     }
 
 
 }
+
+
